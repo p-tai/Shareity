bd2b90db14f078f7e7e49bfd23d079e169851b4d
Tyrfang <javatai@gmail.com>
2014-02-08 18:35:07 -0500

    Added basic front-end templates

--- humbler/views.py
+++ humbler/views.py
@@ -2,8 +2,34 @@ from humbler import app
 from flask import render_template
 import helpers
 
+content = {}
+charities = {}
+
+def listContent():
+    for item in content:
+        print(item)
+
+def listCharities():
+    print("foo");
+    for charity in charities:
+        print(charity)
+
+@app.route('/content')
+
+@app.route('/charities')
+
+@app.route('/url')
+def addContent():
+    print("url")
+    return render_template('buyscreen.html')
+
+@app.route('/addCharity', methods=['POST'])
+def addCharity():
+    return
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """The index page"""
+    listContent();
     return render_template('index.html')
